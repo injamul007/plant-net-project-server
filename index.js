a51e756150e75b0373d7974f938340cfac8c11bc
injamul007 <injamul007hoque@gmail.com>
2025-12-09 00:09:51 +0600

    add and deploy to vercel and customize the env

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const client = new MongoClient(process.env.MONGODB_URI, {
 });
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     //? Database and collection setup
     const db = client.db("plantnetDB");
@@ -540,7 +540,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
